10a5b651622645b20c1970005f6b74e60423cd2d
Simon Whitaker <sw@netcetera.org>
2017-07-30 08:54:54 +0100

    Initial Commit

---
+++ Heap/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Heap
+//
+//  Created by Simon on 30/07/2017.
+//  Copyright © 2017 Simon Whitaker. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
